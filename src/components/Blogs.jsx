c571f86b2cdb89406366aaf78f75d040e8831d0b
ratishjain <ratishjain6@gmail.com>
2023-11-07 21:18:26 +0530

    added blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,40 +1,65 @@
 import { useState, useEffect } from "react";
-
+const query = `
+    {
+      user(username: "Ratishjain") {
+        publication {
+          posts{
+            slug
+            title
+            brief
+            coverImage
+          }
+        }
+      }
+    }
+  `;
 const Blogs = () => {
   const [articles, setArticles] = useState([]);
-  const SECRET_KEY = `${import.meta.env.VITE_HASHNODE_API}`;
-  console.log(SECRET_KEY);
+
+  const fetchPosts = async () => {
+    const response = await fetch("https://api.hashnode.com", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+    const ApiResponse = await response.json();
+
+    console.log(ApiResponse.data.user.publication.posts);
+    setArticles(ApiResponse.data.user.publication.posts);
+    console.log(articles);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.hashnode.com/v1/me/articles",
-          {
-            headers: {
-              Authorization: SECRET_KEY,
-            },
-          }
-        );
-        const data = await response.json();
-        setArticles(data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
+    fetchPosts();
   }, []);
 
   return (
-    <div>
-      {articles.map((article) => (
-        <div key={article._id}>
-          <h2>{article.title}</h2>
-          <p>{article.description}</p>
-          <p>
-            <a href={article.url}>Read more</a>{" "}
-          </p>
-        </div>
-      ))}
+    <div className="p-12" id="blogs">
+      <h1 className="text-center text-3xl mb-2">Blogs</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 ">
+        {articles.map((article) => (
+          <div
+            key={article._id}
+            className="cursor-pointer bg-gray-800 m-4 flex  flex-col gap-1 p-2 rounded-sm"
+          >
+            <img src={article.coverImage} className="rounded-sm" />
+            <div className="flex flex-col justify-evenly gap-1 h-full">
+              <h2 className="text-lg font-bold">{article.title}</h2>
+              <p className="text-xs opacity-40">{article.brief}</p>
+              <p>
+                <a
+                  href={`https://devcon.hashnode.dev/${article.slug}`}
+                  className="text-gray-400 text-lg flex-end"
+                >
+                  Read more...
+                </a>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,9 @@ const Header = () => {
               <Link to="#projects">Projects</Link>
             </li>
             <li>
+              <Link to="#blogs">Blogs</Link>
+            </li>
+            <li>
               <Link
                 to="#contact"
                 className="bg-black text-white px-4 py-2 rounded-full shadow-sm"
