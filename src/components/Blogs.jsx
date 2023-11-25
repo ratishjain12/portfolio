53cd9a50988182793f7163a152430e484e935f3c
ratishjain <ratishjain6@gmail.com>
2023-11-25 18:47:58 +0530

    minute fix

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -42,13 +42,13 @@ const Blogs = () => {
   }, []);
 
   return (
-    <div className="p-12 min-w-[300px]" id="blogs">
+    <div className="p-12" id="blogs">
       <h1 className="text-center text-3xl mb-2">Blogs</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 ">
         {articles.map((article, index) => (
           <div
             key={index}
-            className="cursor-pointer bg-gray-800 m-4 flex  flex-col gap-1 p-2 rounded-sm"
+            className="min-w-1/4 cursor-pointer bg-gray-800 mb-4 mr-2 flex  flex-col gap-1 p-2 rounded-sm"
           >
             <img src={article.node.coverImage.url} className="rounded-sm" />
             <div className="flex flex-col justify-evenly gap-1 h-full">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,32 +7,41 @@ const Header = () => {
         <nav className="h-[50px] mb-2 flex items-center bg-black/30 backdrop-blur-xl ">
           <ul className="flex justify-evenly  max-w-6xl mx-auto flex-1 ">
             <li>
-              <Link to="#bio" className="bg-gray-700 px-4 py-2 rounded-full">
+              <Link
+                to="#bio"
+                className="bg-gray-700 px-3 py-2 rounded-full text-sm"
+              >
                 Bio
               </Link>
             </li>
             <li>
-              <Link to="#skills" className="bg-gray-700 px-4 py-2 rounded-full">
+              <Link
+                to="#skills"
+                className="bg-gray-700 px-4 py-2 rounded-full text-sm"
+              >
                 Skills
               </Link>
             </li>
             <li>
               <Link
                 to="#projects"
-                className="bg-gray-700 px-4 py-2 rounded-full"
+                className="bg-gray-700 px-4 py-2 rounded-full text-sm"
               >
                 Projects
               </Link>
             </li>
             <li>
-              <Link to="#blogs" className="bg-gray-700 px-6 py-2 rounded-full">
+              <Link
+                to="#blogs"
+                className="bg-gray-700 px-6 py-2 rounded-full text-sm"
+              >
                 Blogs
               </Link>
             </li>
             <li>
               <Link
                 to="#contact"
-                className="bg-black text-white px-4 py-2 rounded-full shadow-sm"
+                className="bg-black text-white px-3 py-2 rounded-full shadow-sm"
               >
                 Contact Me
               </Link>
