5bb65d599ffe707690845a4588a785bf3dfbc33c
ratishjain <ratishjain6@gmail.com>
2024-06-01 14:45:49 +0530

    card-fixed(blogs)

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -40,17 +40,17 @@ const Blogs = () => {
   }, []);
 
   return (
-    <div className="p-12" id="blogs">
+    <div className="p-6" id="blogs">
       <div className="header-text mx-auto mb-10 mt-6">
         <h1 className="text-center text-2xl ">Blogs</h1>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 ">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 ">
         {articles.map((article, index) => (
           <div
             key={index}
-            className=" min-w-[33%] cursor-pointer bg-gray-800 mb-4 mr-2 flex  flex-col gap-1 p-2 rounded-sm"
+            className=" cursor-pointer bg-gray-800 mb-4  sm:mr-2 flex  flex-col gap-1 p-2 rounded-sm"
           >
-            <img src={article.node.coverImage.url} className="rounded-sm" />
+            <img src={article.node.coverImage.url} className="rounded-sm " />
             <div className="flex flex-col justify-evenly gap-1 h-full">
               <h2 className="text-lg font-bold">{article.node.title}</h2>
               <p className="text-xs opacity-40">{article.node.brief}</p>
